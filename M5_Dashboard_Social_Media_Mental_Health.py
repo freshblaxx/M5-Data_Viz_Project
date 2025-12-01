8986096a532cd59421de09af7c356e9564d7c2a3
freshblaxx <dominik.fabian.frank@gmail.com>
2025-12-01 09:42:11 -0600

    new requirment

--- M5_Dashboard_Social_Media_Mental_Health.py
+++ M5_Dashboard_Social_Media_Mental_Health.py
@@ -5,6 +5,7 @@ import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
 
+
 # -----------------------------------------------------------------------------
 # Page config
 # -----------------------------------------------------------------------------
